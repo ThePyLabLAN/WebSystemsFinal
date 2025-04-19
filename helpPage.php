8d94d68ef95e29e787d76032f114ecf6cad1eaa9
ThePyLabLAN <62_empiric_psalm@icloud.com>
2025-04-19 19:58:50 -0400

    Update and rename helpPage.html to helpPage.php

--- helpPage.php
+++ helpPage.php
@@ -1,3 +1,30 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'supportDB');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+    die('Connection Failed: ' . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phoneNumber'];
+    $problem = $_POST['problem'];
+    $comms = $_POST['comms'];
+
+    $query = $conn->prepare("INSERT INTO support_requests (firstName, lastName, email, phoneNumber, problem, comms) VALUES (?, ?, ?, ?, ?, ?)");
+    $query->bind_param("ssssss", $firstName, $lastName, $email, $phone, $problem, $comms);
+    $query->execute();
+    $query->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,7 +64,7 @@
     <div class="havingtrouble">
         <p class="httext">Having Trouble? Leave your information below and we will get back with you as soon as possible.</p>
     </div>
-    <form class="middleform" action="submit.php" method="POST">
+    <form class="middleform" action="" method="POST">
 
         <div class="spacing">
             <label for="firstName" class="visually-hidden">First Name:</label>
@@ -116,5 +143,8 @@
             </div>
         </div>
     </div>
+
+
+
 </body>
 </html>
